fda545b9bc8b60f4896a9008148aee4bdc13c59d
VM <104888974+veeeemmm@users.noreply.github.com>
2022-06-09 12:43:51 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('MOVIES', url='https://t.me/+N34LPTnppls0YmNl')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
